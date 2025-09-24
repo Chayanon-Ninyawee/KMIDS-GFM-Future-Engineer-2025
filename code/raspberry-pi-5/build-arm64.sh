67e99633b032c5f072f3fa013f59e56d5a0bdcde
Garfieldcmix <chayanon.nin@gmail.com>
2025-09-25 01:36:05 +0700

    Make lsp work with cross compiling

--- code/raspberry-pi-5/build-arm64.sh
+++ code/raspberry-pi-5/build-arm64.sh
@@ -11,9 +11,42 @@ cp -r "$SCRIPT_DIR/../shared" "$SCRIPT_DIR/src/shared_real"
 docker buildx build -f "$SCRIPT_DIR/Dockerfile.cross" --tag cross-pi .
 
 # Build the project using the compile Dockerfile and output binaries to ./bin
-docker buildx build -f "$SCRIPT_DIR/Dockerfile.compile" -o type=local,dest="$SCRIPT_DIR/build-arm64" .
+docker buildx build -f "$SCRIPT_DIR/Dockerfile.compile" -o type=local,dest="$SCRIPT_DIR/build" .
 
 # Clean up the temporary shared folder
 rm -rf "$SCRIPT_DIR/src/shared_real"
 
-echo "Build complete! Binaries are in $SCRIPT_DIR/bin"
+jq --arg dir "$SCRIPT_DIR" \
+  '
+  map(
+    .directory |= gsub("^/code"; $dir) |
+    .file |= gsub("^/code"; $dir) |
+    .command |= gsub("/code"; $dir)
+  )
+  ' \
+   "$SCRIPT_DIR"/build/compile_commands.json > "$SCRIPT_DIR"/build/compile_commands.json.tmp \
+   && mv "$SCRIPT_DIR"/build/compile_commands.json.tmp "$SCRIPT_DIR"/build/compile_commands.json
+
+jq --arg sysroot "$SCRIPT_DIR/build/aarch64-linux-gnu/include" '
+  map(
+    .command |= gsub("/usr/aarch64-linux-gnu/include/"; ($sysroot + "/"))
+  )
+' "$SCRIPT_DIR/build/compile_commands.json" \
+  > "$SCRIPT_DIR/build/compile_commands.json.tmp" \
+  && mv "$SCRIPT_DIR/build/compile_commands.json.tmp" "$SCRIPT_DIR/build/compile_commands.json"
+
+# Generate .clangd with full absolute paths to cross headers
+SYSROOT="$SCRIPT_DIR/build/aarch64-linux-gnu/include"
+
+cat > "$SCRIPT_DIR/.clangd" <<EOF
+CompileFlags:
+  Add:
+    [
+      -isystem,
+      $SYSROOT/c++/12,
+      -isystem,
+      $SYSROOT/c++/12/aarch64-linux-gnu,
+      -isystem,
+      $SYSROOT
+    ]
+EOF
