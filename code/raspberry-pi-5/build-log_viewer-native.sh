a564217cd6a02b0c59134d7821d9cf34b436d04c
Garfieldcmix <chayanon.nin@gmail.com>
2025-10-01 11:23:18 +0700

    fix: Fix build-log_viewer-native.sh so it remove the correct build_native/ not build/ when clean

--- code/raspberry-pi-5/build-log_viewer-native.sh
+++ code/raspberry-pi-5/build-log_viewer-native.sh
@@ -4,12 +4,11 @@ set -e  # Exit on error
 # If argument is "clean", delete the build directory
 if [ "$1" == "clean" ]; then
     echo "Cleaning build directory..."
-    rm -rf build
+    rm -rf build_native
     exit 0
 fi
 
 # Generate build system if not already present
 cmake -B build_native -DCMAKE_EXPORT_COMPILE_COMMANDS=OFF
 
-# Build all if no target is provided
 cmake --build build_native --target log_viewer -j$(nproc)
